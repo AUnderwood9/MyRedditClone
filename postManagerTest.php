ff4c140a5d8b854a995bf7dbf6fd2440520caa0e
AUnderwood9 <aunderwood797@gmail.com>
2018-09-03 15:54:35 -0500

    Removing unnecessary files.

--- postManagerTest.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <p>
-            Your name: <?php echo $_GET['name'];?>
-            <br />
-            Your color: <?php echo $_GET['color']; ?>
-        </p>
-    </body>
-</html>
