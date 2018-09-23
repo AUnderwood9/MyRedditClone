8fffb437449a69b68fe7f05c2b40ea6c0d178b32
AUnderwood9 <aunderwood797@gmail.com>
2018-09-23 14:29:48 -0500

    Added the comment controller that creates, edit, and deletes comments.\

--- server/src/controllers/commentController.php
+++ server/src/controllers/commentController.php
@@ -8,6 +8,7 @@
         }
 
         /**
+         * @TODO create a logging system instead of echoing out errors to abstract database information away from the front end (Client)
          * The comment is created then its references to the user is created.
          * @param integer postId
          * @param array $commentCreateSet
@@ -15,6 +16,8 @@
          * @return string (enum)
          */
         public function createComment($castId, $postId, $commentCreateSet){
+            $operationStatus = OperationStatusEnum::NONE;
+
             if($_SESSION["sessionUserStatus"]["loginStatus"]){
                 // get current user id
                 try{
@@ -35,19 +38,92 @@
 
                     if($commentCreateResultSet["rowsEffected"] == 1 && $userAssociationResultSet["rowsEffected"] == 1 && $castPostCommentRelationResultSet["rowsEffected"]){
                         $this->dao->commitTransaction();
-                        return OperationStatusEnum::SUCCESS;
+                        $operationStatus = OperationStatusEnum::SUCCESS;
                     } else {
                         $this->dao->rollbackTransaction();
+                        $operationStatus = OperationStatusEnum::FAIL;
                     }
 
 
                 } catch (Exception $e) {
                     echo $e;
                     $this->dao->rollbackTransaction();
+                    $operationStatus = OperationStatusEnum::FAIL;
                 }
             } else {
-                return OperationStatusEnum::NONE;
+                $operationStatus = OperationStatusEnum::NONE;
             }
+            return $operationStatus;
+        }
+
+        /**
+         * @TODO since we are checking to see if the user is logged in a lot this should be abstracted out of the controllers. Or checked in the front end only.
+         * @param $castId integer
+         * @param $postId integer
+         * @param $commentId integer
+         * @param $commentEditSet array
+         *
+         * @return string
+         */
+        function editComment($commentId, $newCommentText) {
+            $operationStatus = OperationStatusEnum::NONE;
+
+            if($_SESSION["sessionUserStatus"]["loginStatus"]) {
+                try {
+                    $this->dao->startTransaction();
+                    $numberOfRowsChanged = $this->dao->updateRecordById("comment", ["content" => $newCommentText], $commentId, "commentId");
+
+                    if($numberOfRowsChanged  == 1){
+                        $this->dao->commitTransaction();
+                        $operationStatus = OperationStatusEnum::SUCCESS;
+                    } else {
+                        $this->dao->rollbackTransaction();
+                        $operationStatus = OperationStatusEnum::FAIL;
+                    }
+
+                } catch (Exception $e) {
+                    echo $e;
+                    $this->dao->rollbackTransaction();
+                    $operationStatus = OperationStatusEnum::FAIL;
+                }
+            } else {
+                $operationStatus = OperationStatusEnum::NONE;
+            }
+            return $operationStatus;
+        }
+
+        /**
+         * @param $castId integer
+         * @param $postId integer
+         * @param $commentId array
+         *
+         * @return string
+         */
+        function deleteComment($commentId) {
+            $operationStatus = OperationStatusEnum::NONE;
+
+            if($_SESSION["sessionUserStatus"]["loginStatus"]) {
+                try {
+                    $this->dao->startTransaction();
+                    $numberOfRowsChanged = $this->dao->deleteRecord("comment", $commentId, "commentId");
+                    
+                    if($numberOfRowsChanged ["Number of Rows Effected"]  == 1){
+                        $this->dao->commitTransaction();
+                        $operationStatus = OperationStatusEnum::SUCCESS;
+                    } else {
+                        $this->dao->rollbackTransaction();
+                        $operationStatus = OperationStatusEnum::FAIL;
+                    }
+
+                } catch (Exception $e) {
+                    echo $e;
+                    $this->dao->rollbackTransaction();
+                    $operationStatus = OperationStatusEnum::FAIL;
+                }
+            } else {
+                $operationStatus = OperationStatusEnum::NONE;
+            }
+            return $operationStatus;
         }
 
     }
