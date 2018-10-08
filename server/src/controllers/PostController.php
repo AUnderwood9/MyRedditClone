979d069df5dee0a9f3fac8edb7ec26f38f3ad076
AUnderwood9 <aunderwood797@gmail.com>
2018-10-07 21:24:43 -0500

    Added transaction management to post creation operations in the post controller.

--- server/src/controllers/PostController.php
+++ server/src/controllers/PostController.php
@@ -17,30 +17,43 @@
          * @return string (enum)
          */
         public function createPost($castId, $postCreateSet){
+			$operationStatus;
             if($_SESSION["sessionUserStatus"]["loginStatus"]){
-                // get current user id
-                $currentUserId = $_SESSION["sessionUserStatus"]["userId"];
+				try{
+					// get current user id
+					$currentUserId = $_SESSION["sessionUserStatus"]["userId"];
 
-                $postCreateSet += ["creatorUserId" => $currentUserId,"likes" => 1, "dislikes" => 0] ;
+					$postCreateSet += ["creatorUserId" => $currentUserId,"likes" => 1, "dislikes" => 0] ;
+	
+					// create post
+					$postCreateResultSet = $this->dao->insertRecord("Post", $postCreateSet);
+	
+					$createdPostId = $postCreateResultSet["effectedId"];
+	
+					// update castPostndex
+					$castPostRelationResultSet = $this->dao->insertRecord("castPostCommentMatrixIndex", ["castId" => $castId, "postId" => $createdPostId]);
+	
+					// update userCreatedContent
+					$userAssociationResultSet = $this->dao->insertRecord("userCreatedContent", ["userId" => $currentUserId, "subCastId" => $castId, "postId" => $createdPostId]);
+	
+					if ($postCreateResultSet["rowsEffected"] == 1 && $userAssociationResultSet["rowsEffected"] == 1 && $castPostRelationResultSet["rowsEffected"] == 1){
+						$this->dao->commitTransaction();
+                        $operationStatus = OperationStatusEnum::SUCCESS;
+                    } else {
+                        $this->dao->rollbackTransaction();
+                        $operationStatus = OperationStatusEnum::FAIL;
+                    }
+				} catch (Exception $e) {
+					$this->dao->rollbackTransaction();
+                    $operationStatus = OperationStatusEnum::FAIL;
 
-                // create post
-                $postCreateResultSet = $this->dao->insertRecord("Post", $postCreateSet);
-
-                $createdPostId = $postCreateResultSet["effectedId"];
-
-                // update castPostndex
-                $castPostRelationResultSet = $this->dao->insertRecord("castPostCommentMatrixIndex", ["castId" => $castId, "postId" => $createdPostId]);
-
-                // update userCreatedContent
-                $userAssociationResultSet = $this->dao->insertRecord("userCreatedContent", ["userId" => $currentUserId, "subCastId" => $castId, "postId" => $createdPostId]);
-
-                if ($userAssociationResultSet["rowsEffected"] == 1 && $castPostRelationResultSet["rowsEffected"] == 1)
-                    return OperationStatusEnum::SUCCESS;
-                else
-                    return OperationStatusEnum::FAIL;
+				}
+                
             } else {
-                return OperationStatusEnum::NONE;
-            }
+                $operationStatus = OperationStatusEnum::NONE;
+			}
+			
+			return $operationStatus;
         }
 
         /**
