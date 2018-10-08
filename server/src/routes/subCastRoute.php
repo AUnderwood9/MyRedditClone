af8234816749490ed2308d6787061e80767e3f24
AUnderwood9 <aunderwood797@gmail.com>
2018-10-07 21:15:25 -0500

    Added the route for managing sub casts.

---
+++ server/src/routes/subCastRoute.php
@@ -0,0 +1,34 @@
+<?php
+
+	use \Psr\Http\Message\ServerRequestInterface as Request;
+	use \Psr\Http\Message\ResponseInterface as Response;
+
+	// $this->get('/casts', function (Request $request, Response $response, array $args) {
+    //     $response->getBody()->write("Hello");
+
+    //     return $response;
+	// });
+
+	$this->get('/casts', function (Request $request, Response $response, array $args) {
+		$castController = new castController(new DaoManager());
+
+		$response->getBody()->write(json_encode($castController->getCastList()));
+
+        return $response;
+	});
+
+	$this->post('/casts', function (Request $request, Response $response, array $args) {
+		$castController = new CastController(new DaoManager());
+		$userController = new UserController(new DaoManager());
+		
+		$requestBody = $request->getParsedBody();
+		$serverResponse = $castController->createCast((array)$requestBody);
+		
+		$responseBody = new StdClass;
+		$responseBody->success = $serverResponse;
+
+		$response->getBody()->write(json_encode($responseBody));
+
+        return $response;
+	});
+?>
