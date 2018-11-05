31ace1e0e0f7263dc4e7c627c06290c9a8cf8bad
AUnderwood9 <aunderwood797@gmail.com>
2018-11-04 21:05:19 -0600

    Added CORS support for all outside domain to routes file. Will have to adjust later for more security.

--- route.php
+++ route.php
@@ -19,11 +19,31 @@
 
     $app = new \Slim\App;
 
+	$app->options('/{routes:.+}', function ($request, $response, $args) {
+		return $response;
+	});
+	
+	$app->add(function ($req, $res, $next) {
+		$response = $next($req, $res);
+		return $response
+				->withHeader('Access-Control-Allow-Origin', '*')
+				->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+				->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+				->withHeader('Access-Control-Allow-Methods', 'Content-Type: application/json');
+	});
 
     $app->group('/api', function ($app) {
 		require_once 'server/src/routes/userRoute.php';
 		require_once 'server/src/routes/subCastRoute.php';
-    });
+		require_once 'server/src/routes/postRoute.php';
+	});
+	
+	// Catch-all route to serve a 404 Not Found page if none of the routes match
+	// NOTE: make sure this route is defined last
+	$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+		$handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+		return $handler($req, $res);
+	});
 
 //    $app->run();
 
